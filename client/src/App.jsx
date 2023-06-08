925af4e12a4d5d566f1780cc376fe7ba0c8ae29a
v-Sharan <vsharansago@gmail.com>
2023-06-08 19:43:24 +0530

    added utils

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { client } from "../utils/client";
+import { AcademicsQueries } from "../utils/queries";
 
 function App() {
   useEffect(() => {
-    axios
-      .get("http://localhost:5050/data/academics")
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err.response.data));
-
-    console.log("hi");
+    client.fetch(AcademicsQueries()).then((data) => console.log(data));
   }, []);
 
   const clickHandler = () => {
