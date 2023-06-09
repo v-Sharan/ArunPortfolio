d588fe6341081b59c1a470723ed0e556dcb8410e
v-Sharan <vsharansago@gmail.com>
2023-06-09 16:30:36 +0530

    added env variables

--- client/utils/client.js
+++ client/utils/client.js
@@ -1,10 +1,9 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "25ann58w",
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
-  token:
-    "skWtpwzumEBdMLlpwNnJMcWgDlBev2LTK8lgF7nAWtKbBjll9c4bXvCdnIOAQxQYcLuPKT98kHfKOLu20A9ycegSHel2VcksYdq5czguLnjyWcqfTnFP7HAX228GgMXLjf7RBzkWIyzQMQYPvuMXIgO6foHW8XZzFBtUrHZPboJt79Km55K6",
+  token: import.meta.env.VITE_SANITY_TOKEN,
 });
